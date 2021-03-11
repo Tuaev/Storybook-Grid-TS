27c1b90d7cd921e32e5073e9b4be4cbd8171be11
Soslan <sparksoslan@gmail.com>
2021-03-11 18:23:02 +0000

    Add justify content and align items to Grid

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,32 @@ import GridRuler from './components/GridRuler/GridRuler';
 
 function App() {
   return (
-    <div className="App" style={{ margin: '16px', position: 'relative' }}>
+    <div
+      className="App"
+      style={{ margin: '16px', position: 'relative', height: '100vh' }}
+    >
       <GridRuler spacing="sm"></GridRuler>
-      <Grid container spacing="sm">
-        <Grid item xs={12} sm={6} md={4} lg={3}>
+
+      <Grid
+        container
+        spacing="sm"
+        justifyContent="center"
+        alignItems="center"
+        style={{ height: '100%' }}
+      >
+        <Grid item xs={1} sm={6} md={4} lg={3}>
           <Box>Box 1</Box>
         </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
+        <Grid item xs={1} sm={6} md={4} lg={3}>
           <Box>Box 2</Box>
         </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
+        <Grid item xs={1} sm={6} md={4} lg={3}>
           <Box>Box 3</Box>
         </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
+        <Grid item xs={1} sm={6} md={4} lg={3}>
           <Box>Box 4</Box>
         </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
+        <Grid item xs={1} sm={6} md={4} lg={3}>
           <Box>Box 5</Box>
         </Grid>
       </Grid>
--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,21 +1,39 @@
-import { FunctionComponent, ReactNode } from 'react';
+import { FunctionComponent, HTMLAttributes, ReactNode } from 'react';
 import cn from 'classnames';
 import styles from './Grid.module.scss';
 
 type Cols = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 type Spacing = 'sm' | 'md' | 'lg';
+type JustifyContent = 'flex-start' | 'center' | 'flex-end' | 'space-between';
+type AlignItems = 'flex-start' | 'center' | 'flex-end';
 
-const Grid: FunctionComponent<{
-  children: ReactNode;
-  container?: boolean;
-  item?: boolean;
-  cols?: Cols;
-  spacing?: Spacing;
-  xs?: Cols;
-  sm?: Cols;
-  md?: Cols;
-  lg?: Cols;
-}> = ({ children, container, item, xs, sm, md, lg, spacing }) => {
+const Grid: FunctionComponent<
+  {
+    children: ReactNode;
+    container?: boolean;
+    item?: boolean;
+    cols?: Cols;
+    xs?: Cols;
+    sm?: Cols;
+    md?: Cols;
+    lg?: Cols;
+    spacing?: Spacing;
+    justifyContent?: JustifyContent;
+    alignItems?: AlignItems;
+  } & HTMLAttributes<HTMLDivElement>
+> = ({
+  children,
+  container,
+  item,
+  xs,
+  sm,
+  md,
+  lg,
+  spacing,
+  justifyContent,
+  alignItems,
+  ...props
+}) => {
   const classNames = cn({
     [styles.Grid_container]: container,
     [styles.Grid_item]: item,
@@ -24,8 +42,14 @@ const Grid: FunctionComponent<{
     [styles[`Grid_md_${md}`]]: md,
     [styles[`Grid_lg_${lg}`]]: lg,
     [styles[`Grid_spacing_${spacing}`]]: spacing,
+    [styles[`Grid_justifyContent_${justifyContent}`]]: justifyContent,
+    [styles[`Grid_alignItems_${alignItems}`]]: alignItems,
   });
-  return <div className={classNames}>{children}</div>;
+  return (
+    <div className={classNames} {...props}>
+      {children}
+    </div>
+  );
 };
 
 export default Grid;
